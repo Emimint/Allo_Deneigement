2416b3789e8bd4c39d59fb0f2c966c2e91d30b8b
Emimint <90863470+Emimint@users.noreply.github.com>
2024-01-13 22:30:48 -0500

    add "Supprimer" option in table

--- src/main/resources/static/scripts/load-table.js
+++ src/main/resources/static/scripts/load-table.js
@@ -9,7 +9,14 @@ $(document).ready(function() {
             targets: 6,
             render: function (data, type, row, meta)
             {
-                data = '<a href="/fournisseur"><i class="fa-sharp fa-solid fa-circle-info" style="color: #b50303;"></i></a>';
+                data = '<div style="width:100%; display:flex; justify-content: space-around;"><a href="/fournisseur"><i class="fa-sharp fa-solid fa-circle-info" style="color: #b50303;"></i></a></div>';
+                return data;
+            }
+        },
+        {
+            targets: 7, // Index of the new column
+            render: function (data, type, row, meta) {
+                data = '<div style="width:100%; display:flex; justify-content: space-around;"><button"><i class="fa-solid fa-trash" style="color: #b50303;"></i></button></div>';
                 return data;
             }
         }],
