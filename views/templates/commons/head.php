cd16083e87409af539559e33f0ca54c065861015
Emimint <90863470+Emimint@users.noreply.github.com>
2024-01-22 18:08:54 -0500

    fix some more paths

--- views/templates/commons/head.php
+++ views/templates/commons/head.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8" />
     <title>Allo Déneigement</title>
-    <link rel="icon" href="static/image/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="<?php echo BASE_URL;?>/static/image/favicon.ico" type="image/x-icon">
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <!-- CSS LIBRAIRIES -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"/>
