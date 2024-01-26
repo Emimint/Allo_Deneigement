45a31bfc067a2e3be56ddadf5b09f949d99465f8
Emimint <90863470+Emimint@users.noreply.github.com>
2024-01-26 13:45:03 -0500

    update submission form

--- views/templates/pages/soumission-offre.php
+++ views/templates/pages/soumission-offre.php
@@ -5,7 +5,7 @@
 
   <!-- Modal for adresse: -->
   <div class="modal fade" id="exampleModalCenter" tabindex="-1" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-dialog modal-dialog-centered p-2">
       <div class="modal-content">
         <div class="modal-header">
           <h5 class="modal-title" id="exampleModalCenterTitle">Adresse de livraison</h5>
@@ -18,44 +18,37 @@
               <input type="text" class="form-control" id="inputAddress" placeholder="rue Principale">
             </div>
 
-            <div class="col-md-2">
+            <div class="col-md-4">
               <label for="inputAddress2" class="form-label">Appartement</label>
               <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, etage">
             </div>
 
-            <div class="col-md-6">
-              <label for="inputCity" class="form-label">Ville</label>
-              <input type="text" class="form-control" id="inputCity">
-            </div>
-
-            <div class="col-md-4">
-              <label for="inputState" class="form-label">State</label>
-              <select id="inputState" class="form-select">
-                <option selected>Choose...</option>
-                <option>...</option>
-              </select>
-            </div>
-
-            <div class="col-md-2">
-              <label for="inputZip" class="form-label">Zip</label>
-              <input type="text" class="form-control" id="inputZip">
-            </div>
-
-            <div class="col-12">
-              <div class="form-check">
-                <input class="form-check-input" type="checkbox" id="gridCheck">
-                <label class="form-check-label" for="gridCheck">Remember me</label>
+            <div class="d-flex align-items-end">
+              <div class="col-sm-6 mx-1">
+                <label for="ville">Ville</label>
+                <input class="form-control" name="ville" placeholder="Montreal" />
+                <nordpass-icon data-np-uid="cb62c672-0256-4aa3-be50-a7bc133f49bd"></nordpass-icon>
+              </div>
+              <div class="col-sm-3 mx-1">
+                <label for="PostalCode">Code Postal</label>
+                <input class="form-control" name="PostalCode" placeholder="A1B 2C3" />
+              </div>
+              <div class="col-sm-3 mx-1">
+                <label for="Province">Province</label>
+                <select class="form-control">
+                  <option>Quebec</option>
+                  <option>Ontario</option>
+                  <option>...</option>
+                </select>
               </div>
             </div>
 
-            <div class="col-12">
-              <button type="submit" class="btn btn-primary">Submit</button>
+
+            <div class="col-12 mt-5">
+              <button type="submit" class="btn btn-primary">Soumettre</button>
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fermer</button>
             </div>
           </form>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary">Save changes</button>
         </div>
       </div>
     </div>
@@ -117,23 +110,65 @@
     </div>
 
     <div class="step step-3 m-2">
-      <h3>Step 3</h3>
+      <h3>Infos sur le service</h3>
       <div class="mb-3">
-        <label for="field3" class="form-label">Field 3:</label>
-        <input type="text" class="form-control" id="field3" name="field3">
+        <label for="field3" class="form-label">Veuillez donner des precisions sur le service: [Nombre approximatif de tonne de neige a deplacer] </label>
+        <select id="field3" class="form-control" name="field3" required>
+          <option> Moins de 500kg</option>
+          <option>De 500kg a 1 tonne </option>
+          <option>Plus d'une tonne</option>
+        </select>
       </div>
       <button type="button" class="btn prev-step">Precedent</button>
       <button type="button" class="btn next-step">Suivant</button>
     </div>
 
     <div class="step step-4 m-2">
-      <h3>Step 4</h3>
-      <div class="mb-3">
-        <label for="field4" class="form-label">Field 4:</label>
-        <input type="text" class="form-control" id="field4" name="field4">
+      <div class="mb-3 recapitulatif">
+        <label class="h2" for="field4" class="form-label">Recapitulatif de votre commande</label>
+        <!-- <input type="text" class="form-control" id="field4" name="field4"> -->
+        <div class="container mt-3">
+          <h2>Votre service</h2>
+          <ul class="list-group">
+            <li class="list-group-item">[Nom du service]</li>
+          </ul>
+          <h2>Adresse de livraison</h2>
+          <ul class="list-group">
+            <li class="list-group-item">[Adresse]</li>
+            <li class="list-group-item">[Adresse 2]</li>
+            <li class="list-group-item">[Adresse 3]</li>
+          </ul>
+          <h2>Facturation</h2>
+          <div class="container">
+            <table class="table">
+              <thead>
+                <tr>
+                  <th scope="col">Service</th>
+                  <th scope="col">[Litrage/Tonnage/ect...]</th>
+                  <th scope="col">Prix unitaire</th>
+                  <th scope="col">Total</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">[Nom service]</th>
+                  <td>[moins dune tonne]</td>
+                  <td>100$</td>
+                  <td>100$</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <div class="mb-3">
+          <label for="commentaire-soumission" class="form-label">Ajoutez un commentaire (demande de details, informations importantes, ...):</label>
+          <textarea class="form-control" id="commentaire-soumission" rows="3"></textarea>
+        </div>
       </div>
-      <button type="button" class="btn prev-step">Precedent</button>
-      <button type="submit" class="btn">Soumettre</button>
+      <div class="d-flex justify-content-end">
+        <button type="button" class="btn m-2 prev-step">Precedent</button>
+        <button type="submit" class="btn m-2">Soumettre</button>
+      </div>
     </div>
   </form>
 </div>
