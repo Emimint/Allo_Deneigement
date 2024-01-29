e630aff764c636bd1ad3526fb71548407386df92
f-zahra <eva-200@hotmail.fr>
2024-01-28 19:50:08 -0500

    Update navbar.php

--- views/templates/commons/navbar.php
+++ views/templates/commons/navbar.php
@@ -1,48 +1,6 @@
-<?php if (!defined('BASE_URL')) define('BASE_URL', 'http://localhost:80/Allo_Deneigement/views/'); ?>
-    <div>
-        <header class="p-3 text-white" style="background-color: #b50303">
-            <div class="container-fluid">
-            <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-            <a href="<?php echo BASE_URL;?>" class="d-flex align-items-center mb-3 mb-lg-0 text-white text-decoration-none me-lg-auto">
-            <div class="d-flex align-items-center bd-highlight ">
-            <img class="mb-4 mt-3 img-fluid img-logo" src="<?php echo BASE_URL;?>/static/image/flocon-black.svg" style="height: 80px; width: 80px;" alt="LOGO">
-            <h4 >ALLO<br> DÉNEIGEMENT
-            </div>
-            </a>
-            <ul class="d-flex  align-items-center nav col-12  col-lg-auto mb-2 mb-md-0">
-            <li class="nav-link" ><a href="<?php echo BASE_URL;?>" class="nav-link px-2 text-white">A propos</a></li>
-            <li  class="nav-link"><a href="<?php echo BASE_URL;?>/templates/pages/historique-utilisateur.php" class="nav-link px-2 text-white">Mes demandes de service</a></li>
-            <li class="nav-link"><a href="<?php echo BASE_URL;?>/templates/pages/contactPage.php" class="nav-link px-2 text-white">Nous joindre</a></li>
-            <li class="nav-link"><a href="#" class="nav-link px-2"><i class="fa-regular fa-bell" style="color: #ffffff;"></i></a></li>
-            <li class="nav-link dropdown">
-            <a href="#" class="nav-link px-2 dropdown-toggle" id="languageDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            <i class="fa-solid fa-globe" style="color: #ffffff;"></i>
-            </a>
-            <ul class="dropdown-menu" aria-labelledby="languageDropdown">
-            <li><a class="dropdown-item" href="?lang=en">English</a></li>
-            <li><a class="dropdown-item" href="?lang=fr">Français</a></li>
-            </ul>
-            </li>
-            <li></li>
-            <li class="">
-                <?php
-             
-                
-                // Check if the user is logged in
-                if (false) {
-                    // User is logged in, display user icon and a logout link
-                    echo '
-                    <a href="#" class="nav-link px-2 dropdown-toggle" id="languageDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    <span style="color:white">Hello username</a>
-                    <ul class="dropdown-menu" aria-labelledby="languageDropdown">
-                    <li><a class="dropdown-item" href="?lang=en"><a class="nav-link" href="'. BASE_URL .'templates/pages/login.php">Logout</a></a></li>
-                   
-                    </ul>
-                    </span>';
-
-                } else {
-                    // User is not logged in, display the login button
-                    echo '<a href="'. BASE_URL .'templates/pages/login.php"><button type="button" class="btn btn-outline-light">Connexion</button></a>';
+<?php
+if (!defined('BASE_URL')) define('BASE_URL', 'http://localhost:80/Allo_Deneigement/views/');
+?>
 
 <div>
     <header class="p-3 text-white" style="background-color: #b50303">
