c97fdaacccc3cb390cfbd86013910f707a8582bf
f-zahra <eva-200@hotmail.fr>
2024-02-11 16:49:57 -0500

    Updated file

--- models/Fournisseur.class.php
+++ models/Fournisseur.class.php
@@ -2,7 +2,7 @@
 
 class Fournisseur extends Personne {
     private $nom_de_la_compagnie;
-    private $note_globale;
+    private $note_globale=[];
 
     public function getNomDeLaCompagnie() {
         return $this->nom_de_la_compagnie;
@@ -25,7 +25,19 @@ class Fournisseur extends Personne {
         return "Fournisseur: {$this->nom} {$this->prenom}, Compagnie: {$this->nom_de_la_compagnie}, Note Globale: {$this->note_globale}";
     }
 
+    
+    public function ajouterNote($rating) {
+        $this->note_globale[] = $rating;
+    }
 
-    public function calculeNoteGlobale(){}
+    public function calculeNoteGlobale(){
+        
+        if (count($this->note_globale) > 0) {
+            $averageRating = array_sum($this->note_globale) / count($this->note_globale);
+            return round($averageRating, 2);
+        } else {
+            return 0;
+        }
+    }
 
 }
