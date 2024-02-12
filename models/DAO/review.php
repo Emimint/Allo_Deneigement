fefecc4d950d59467ecf14e00f8f359e9e8c809f
cylia93 <cylia.oudiai@outlook.com>
2024-02-12 16:29:38 -0500

    update review class

--- models/DAO/review.php
+++ models/DAO/review.php
@@ -4,65 +4,70 @@ class Review {
     private $id_review;
     private $score;
     private $commentaire;
-    private $liste_photos;
     private $id_utilisateur;
+    private $id_service;
     private $date_commentaire;
 
-    public function __construct($id_review, $score, $commentaire, $liste_photos, $id_utilisateur, $date_commentaire) {
+    // Constructeur
+    public function __construct($id_review, $score, $commentaire, $id_utilisateur, $id_service, $date_commentaire) {
         $this->id_review = $id_review;
         $this->score = $score;
         $this->commentaire = $commentaire;
-        $this->liste_photos = $liste_photos;
         $this->id_utilisateur = $id_utilisateur;
+        $this->id_service = $id_service;
         $this->date_commentaire = $date_commentaire;
     }
 
-    // Getters et Setters
+    // Méthodes getter
     public function getIdReview() {
         return $this->id_review;
-    }
-
-    public function setIdReview($id_review) {
-        $this->id_review = $id_review;
     }
 
     public function getScore() {
         return $this->score;
     }
 
-    public function setScore($score) {
-        $this->score = $score;
-    }
-
     public function getCommentaire() {
         return $this->commentaire;
-    }
-
-    public function setCommentaire($commentaire) {
-        $this->commentaire = $commentaire;
-    }
-
-    public function getListePhotos() {
-        return $this->liste_photos;
-    }
-
-    public function setListePhotos($liste_photos) {
-        $this->liste_photos = $liste_photos;
     }
 
     public function getIdUtilisateur() {
         return $this->id_utilisateur;
     }
 
-    public function setIdUtilisateur($id_utilisateur) {
-        $this->id_utilisateur = $id_utilisateur;
+    public function getIdService() {
+        return $this->id_service;
     }
 
     public function getDateCommentaire() {
         return $this->date_commentaire;
     }
 
+    // Méthodes setter
+    public function setScore($score) {
+        $this->score = $score;
+    }
+
+    public function setCommentaire($commentaire) {
+        $this->commentaire = $commentaire;
+    }
+
+    public function setIdUtilisateur($id_utilisateur) {
+        $this->id_utilisateur = $id_utilisateur;
+    }
+
+    public function setIdService($id_service) {
+        $this->id_service = $id_service;
+    }
+
     public function setDateCommentaire($date_commentaire) {
         $this->date_commentaire = $date_commentaire;
     }
 }
+
+public function __toString() {
+    return "ID Review: " . $this->id_review . ", Score: " . $this->score . ", Commentaire: " . $this->commentaire . ", ID Utilisateur: " . $this->id_utilisateur . ", ID Service: " . $this->id_service . ", Date Commentaire: " . $this->date_commentaire;
+}
+
+
+?>
