9397bdf2d25fe984b102ef8baabb2011f16d429d
cylia93 <cylia.oudiai@outlook.com>
2024-02-11 21:09:04 -0500

    update class administrateur

--- models/administrateur.php
+++ models/administrateur.php
@@ -1,46 +1,28 @@
 <?php
 
 class Administrateur {
-   
+    private $id_administrateur;
+    private $nom;
+    private $prenom;
+    private $telephone;
+    private $username;
+    private $password;
+    private $photo_url;
 
     // Constructeur
-    public function __construct($id, $nom, $prenom, $email, $password, $addresse, $telephone, $username, $photo_url) {
-        $this->id = $id;
+    public function __construct($id_administrateur, $nom, $prenom, $telephone, $username, $password, $photo_url) {
+        $this->id_administrateur = $id_administrateur;
         $this->nom = $nom;
         $this->prenom = $prenom;
-        $this->email = $email;
-        $this->adresse = $adresse;
         $this->telephone = $telephone;
         $this->username = $username;
         $this->password = $password;
-        $this->photo_ur = $photo_ur;
+        $this->photo_url = $photo_url;
     }
 
-   // fontion modier compte
-    public function modifierCompte($idCompte, $nouveauUsername, $nouveauPrenom, $nouvelEmail, $nouveauPassword) {}
-    
-     // fontion trouver compte
-    public function trouverCompte($idCompte) { }
-
-     // fontion supprimer compte
-    public function supprimerCompte($idCompte) {}
-
-    // Méthodes pour gérer les offres de service
-
-     // fontion ajouter offre
-    public function ajouterOffre($offreService) {}
-
-
-     // fontion trouver offre
-    public function trouverOffre($idOffre) {  }
-    
-    // fontion supprimer offre
-    public function supprimerOffre($idOffre) {}
-    
-
-    // Getters
-    public function getId() {
-        return $this->id;
+    // Méthodes getter
+    public function getIdAdministrateur() {
+        return $this->id_administrateur;
     }
 
     public function getNom() {
@@ -51,23 +33,23 @@ class Administrateur {
         return $this->prenom;
     }
 
-    public function getEmail() {
-        return $this->email;
+    public function getTelephone() {
+        return $this->telephone;
+    }
+
+    public function getUsername() {
+        return $this->username;
     }
 
     public function getPassword() {
         return $this->password;
     }
 
-    public function getAdresse() {
-        return $this->adresse;
+    public function getPhotoUrl() {
+        return $this->photo_url;
     }
 
-    public function getTelephone() {
-        return $this->telephone;
-    }
-
-    // Setters
+    // Méthodes setter
     public function setNom($nom) {
         $this->nom = $nom;
     }
@@ -76,26 +58,25 @@ class Administrateur {
         $this->prenom = $prenom;
     }
 
-    public function setEmail($email) {
-        $this->email = $email;
+    public function setTelephone($telephone) {
+        $this->telephone = $telephone;
+    }
+
+    public function setUsername($username) {
+        $this->username = $username;
     }
 
     public function setPassword($password) {
         $this->password = $password;
     }
 
-    public function setAdresse($adresse) {
-        $this->adresse = $adresse;
+    public function setPhotoUrl($photo_url) {
+        $this->photo_url = $photo_url;
     }
 
-    public function setTelephone($telephone) {
-        $this->telephone = $telephone;
-    }
-
-    public function __toString(){
-
-        return "ID: " . $this->id ,", Utilisateur: ".$this->username ,", Mot de passe: " .$this->password , ", Nom: " . $this->nom . ", Prénom: " . $this->prenom . ", Email: " . $this->email ",Adresse: ".$this->adresse ",Telephone: " .$this->telephone ", photo: " .$this->photo_url ;
-       
+    // Méthode magique pour convertir l'objet en chaîne de caractères
+    public function __toString() {
+        return "ID: " . $this->id_administrateur . ", Nom: " . $this->nom . ", Prénom: " . $this->prenom . ", Téléphone: " . $this->telephone . ", Username: " . $this->username . ", Photo URL: " . $this->photo_url;
     }
 }
 
