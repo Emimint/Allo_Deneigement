8d67f9f9baea4abbefa5a28d1f4e6ab7cae821d0
f-zahra <eva-200@hotmail.fr>
2024-02-12 13:44:41 -0500

    Updated fournisseur class

--- models/Fournisseur.class.php
+++ models/Fournisseur.class.php
@@ -1,8 +1,30 @@
 <?php
 
-class Fournisseur extends Personne {
+class Fournisseur{
+    protected $id;
+    protected $nom;
+    protected $prenom;
+    protected $adresse; 
+    protected $telephone;
+    protected $username;
+    protected $password;
+    protected $photo_url;
+    protected $adresses;
     private $nom_de_la_compagnie;
-    private $note_globale=[];
+    private $note_globale = [];
+
+    public function __construct($id, $nom, $prenom, $adresse, $telephone, $username, $password, $photo_url, $nom_de_la_compagnie, $note_globale) {
+        $this->id = $id;
+        $this->nom = $nom;
+        $this->prenom = $prenom;
+        $this->adresse = $adresse;
+        $this->telephone = $telephone;
+        $this->username = $username;
+        $this->password = $password;
+        $this->photo_url = $photo_url;
+        $this->nom_de_la_compagnie = $nom_de_la_compagnie;
+        $this->note_globale = $note_globale;
+    }
 
     public function getNomDeLaCompagnie() {
         return $this->nom_de_la_compagnie;
@@ -25,13 +47,11 @@ class Fournisseur extends Personne {
         return "Fournisseur: {$this->nom} {$this->prenom}, Compagnie: {$this->nom_de_la_compagnie}, Note Globale: {$this->note_globale}";
     }
 
-    
     public function ajouterNote($rating) {
         $this->note_globale[] = $rating;
     }
 
-    public function calculeNoteGlobale(){
-        
+    public function calculeNoteGlobale() {
         if (count($this->note_globale) > 0) {
             $averageRating = array_sum($this->note_globale) / count($this->note_globale);
             return round($averageRating, 2);
@@ -39,5 +59,4 @@ class Fournisseur extends Personne {
             return 0;
         }
     }
-
 }
