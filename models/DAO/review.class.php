dff56a8f5c7d9b652064f4cab56525267d208e65
Emimint <90863470+Emimint@users.noreply.github.com>
2024-02-13 11:38:19 -0500

    fix error
    toString function was outsise the class

--- models/DAO/review.class.php
+++ models/DAO/review.class.php
@@ -1,6 +1,7 @@
 <?php
 
-class Review {
+class Review
+{
     private $id_review;
     private $score;
     private $commentaire;
@@ -9,7 +10,8 @@ class Review {
     private $date_commentaire;
 
     // Constructeur
-    public function __construct($id_review, $score, $commentaire, $id_utilisateur, $id_service, $date_commentaire) {
+    public function __construct($id_review, $score, $commentaire, $id_utilisateur, $id_service, $date_commentaire)
+    {
         $this->id_review = $id_review;
         $this->score = $score;
         $this->commentaire = $commentaire;
@@ -19,55 +21,63 @@ class Review {
     }
 
     // Méthodes getter
-    public function getIdReview() {
+    public function getIdReview()
+    {
         return $this->id_review;
     }
 
-    public function getScore() {
+    public function getScore()
+    {
         return $this->score;
     }
 
-    public function getCommentaire() {
+    public function getCommentaire()
+    {
         return $this->commentaire;
     }
 
-    public function getIdUtilisateur() {
+    public function getIdUtilisateur()
+    {
         return $this->id_utilisateur;
     }
 
-    public function getIdService() {
+    public function getIdService()
+    {
         return $this->id_service;
     }
 
-    public function getDateCommentaire() {
+    public function getDateCommentaire()
+    {
         return $this->date_commentaire;
     }
 
     // Méthodes setter
-    public function setScore($score) {
+    public function setScore($score)
+    {
         $this->score = $score;
     }
 
-    public function setCommentaire($commentaire) {
+    public function setCommentaire($commentaire)
+    {
         $this->commentaire = $commentaire;
     }
 
-    public function setIdUtilisateur($id_utilisateur) {
+    public function setIdUtilisateur($id_utilisateur)
+    {
         $this->id_utilisateur = $id_utilisateur;
     }
 
-    public function setIdService($id_service) {
+    public function setIdService($id_service)
+    {
         $this->id_service = $id_service;
     }
 
-    public function setDateCommentaire($date_commentaire) {
+    public function setDateCommentaire($date_commentaire)
+    {
         $this->date_commentaire = $date_commentaire;
     }
+    public function __toString()
+    {
+        return "ID Review: " . $this->id_review . ", Score: " . $this->score . ", Commentaire: " . $this->commentaire . ", ID Utilisateur: " . $this->id_utilisateur . ", ID Service: " . $this->id_service . ", Date Commentaire: " . $this->date_commentaire;
+    }
 }
-
-public function __toString() {
-    return "ID Review: " . $this->id_review . ", Score: " . $this->score . ", Commentaire: " . $this->commentaire . ", ID Utilisateur: " . $this->id_utilisateur . ", ID Service: " . $this->id_service . ", Date Commentaire: " . $this->date_commentaire;
-}
-
-
-?>
