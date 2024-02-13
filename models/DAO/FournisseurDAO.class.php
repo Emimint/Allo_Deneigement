9473c3ef35dbf26175c3205d12e93412a53bd095
f-zahra <eva-200@hotmail.fr>
2024-02-12 21:20:38 -0500

    Updated Fournisseur class and FournisseurDAO

--- models/DAO/FournisseurDAO.class.php
+++ models/DAO/FournisseurDAO.class.php
@@ -7,49 +7,45 @@ if (defined("DOSSIER_BASE_INCLUDE") == false) {
 include_once(DOSSIER_BASE_INCLUDE . "models/DAO/DAO.interface.php");
 include_once(DOSSIER_BASE_INCLUDE . "models/Fournisseur.class.php");
 
-class FournisseurDAO implements DAO
-{
-    public static function chercher($cles)
-    {
+class FournisseurDAO  implements DAO{
+    public static function chercher($id_fournisseur) {
         try {
             $connexion = ConnexionBD::getInstance();
         } catch (Exception $e) {
             throw new Exception("Impossible d’obtenir la connexion à la BD.");
         }
 
-        $unFournisseur = null;
+        $fournisseur = null;
 
         $requete = $connexion->prepare("SELECT * FROM Fournisseur WHERE id_fournisseur=?");
-        $requete->execute(array($cles));
+        $requete->execute(array($id_fournisseur));
 
         if ($requete->rowCount() != 0) {
             $enr = $requete->fetch();
-            $unFournisseur = new Fournisseur(
-                $enr['id_fournisseur'],
-                $enr['nom_contact'],
-                $enr['prenom_contact'],
-                $enr['telephone'],
-                $enr['username'],
-                $enr['password'],
-                $enr['photo_url'],
-                $enr['nom_de_la_compagnie'],
-                $enr['note_globale']
-            );
+            $fournisseur = new Fournisseur();
+            $fournisseur->setIdFournisseur($enr['id_fournisseur']);
+            $fournisseur->setEmail($enr['email']);
+            $fournisseur->setNomDeLaCompagnie($enr['nom_de_la_compagnie']);
+            $fournisseur->setNomContact($enr['nom_contact']);
+            $fournisseur->setPrenomContact($enr['prenom_contact']);
+            $fournisseur->setTelephone($enr['telephone']);
+            $fournisseur->setUsername($enr['username']);
+            $fournisseur->setPassword($enr['password']);
+            $fournisseur->setPhotoUrl($enr['photo_url']);
+            $fournisseur->setNoteGlobale($enr['note_globale']);
         }
 
         $requete->closeCursor();
         ConnexionBD::close();
 
-        return $unFournisseur;
+        return $fournisseur;
     }
 
-    public static function chercherTous()
-    {
+    public static function chercherTous() {
         return self::chercherAvecFiltre("");
     }
 
-    public static function chercherAvecFiltre($filtre)
-    {
+    public static function chercherAvecFiltre($filtre) {
         try {
             $connexion = ConnexionBD::getInstance();
         } catch (Exception $e) {
@@ -62,18 +58,18 @@ class FournisseurDAO implements DAO
         $requete->execute();
 
         foreach ($requete as $enr) {
-            $unFournisseur = new Fournisseur(
-                $enr['id_fournisseur'],
-                $enr['nom_contact'],
-                $enr['prenom_contact'],
-                $enr['telephone'],
-                $enr['username'],
-                $enr['password'],
-                $enr['photo_url'],
-                $enr['nom_de_la_compagnie'],
-                $enr['note_globale']
-            );
-            array_push($tableau, $unFournisseur);
+            $fournisseur = new Fournisseur();
+            $fournisseur->setIdFournisseur($enr['id_fournisseur']);
+            $fournisseur->setEmail($enr['email']);
+            $fournisseur->setNomDeLaCompagnie($enr['nom_de_la_compagnie']);
+            $fournisseur->setNomContact($enr['nom_contact']);
+            $fournisseur->setPrenomContact($enr['prenom_contact']);
+            $fournisseur->setTelephone($enr['telephone']);
+            $fournisseur->setUsername($enr['username']);
+            $fournisseur->setPassword($enr['password']);
+            $fournisseur->setPhotoUrl($enr['photo_url']);
+            $fournisseur->setNoteGlobale($enr['note_globale']);
+            array_push($tableau, $fournisseur);
         }
 
         $requete->closeCursor();
@@ -82,49 +78,56 @@ class FournisseurDAO implements DAO
         return $tableau;
     }
 
-    public static function inserer($unFournisseur)
-    {
+    public static function inserer($fournisseur) {
         try {
             $connexion = ConnexionBD::getInstance();
         } catch (Exception $e) {
             throw new Exception("Impossible d’obtenir la connexion à la BD.");
         }
 
-        $requete = $connexion->prepare("INSERT INTO Fournisseur (nom_contact, prenom_contact, nom_de_la_compagnie, note_globale) VALUES (?, ?, ?, ?)");
+        $requete = $connexion->prepare("INSERT INTO Fournisseur (email, nom_de_la_compagnie, nom_contact, prenom_contact, telephone, username, password, photo_url, note_globale) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
 
         $tableauInfos = [
-            $unFournisseur->getNom(),
-            $unFournisseur->getPrenom(),
-            $unFournisseur->getNomDeLaCompagnie(),
-            $unFournisseur->getNoteGlobale()
+            $fournisseur->getEmail(),
+            $fournisseur->getNomDeLaCompagnie(),
+            $fournisseur->getNomContact(),
+            $fournisseur->getPrenomContact(),
+            $fournisseur->getTelephone(),
+            $fournisseur->getUsername(),
+            $fournisseur->getPassword(),
+            $fournisseur->getPhotoUrl(),
+            $fournisseur->getNoteGlobale()
         ];
 
         return $requete->execute($tableauInfos);
     }
 
-    public static function modifier($unFournisseur)
-    {
+    public static function modifier($fournisseur) {
         try {
             $connexion = ConnexionBD::getInstance();
         } catch (Exception $e) {
             throw new Exception("Impossible d’obtenir la connexion à la BD.");
         }
 
-        $requete = $connexion->prepare("UPDATE Fournisseur SET nom_contact=?, prenom_contact=?, nom_de_la_compagnie=?, note_globale=? WHERE id_fournisseur=?");
+        $requete = $connexion->prepare("UPDATE Fournisseur SET email=?, nom_de_la_compagnie=?, nom_contact=?, prenom_contact=?, telephone=?, username=?, password=?, photo_url=?, note_globale=? WHERE id_fournisseur=?");
 
         $tableauInfos = [
-            $unFournisseur->getNom(),
-            $unFournisseur->getPrenom(),
-            $unFournisseur->getNomDeLaCompagnie(),
-            $unFournisseur->getNoteGlobale(),
-            $unFournisseur->getIdFournisseur()
+            $fournisseur->getEmail(),
+            $fournisseur->getNomDeLaCompagnie(),
+            $fournisseur->getNomContact(),
+            $fournisseur->getPrenomContact(),
+            $fournisseur->getTelephone(),
+            $fournisseur->getUsername(),
+            $fournisseur->getPassword(),
+            $fournisseur->getPhotoUrl(),
+            $fournisseur->getNoteGlobale(),
+            $fournisseur->getIdFournisseur()
         ];
 
         return $requete->execute($tableauInfos);
     }
 
-    public static function supprimer($unFournisseur)
-    {
+    public static function supprimer($fournisseur) {
         try {
             $connexion = ConnexionBD::getInstance();
         } catch (Exception $e) {
@@ -133,27 +136,8 @@ class FournisseurDAO implements DAO
 
         $requete = $connexion->prepare("DELETE FROM Fournisseur WHERE id_fournisseur=?");
 
-        $tableauInfos = [$unFournisseur->getIdFournisseur()];
+        $tableauInfos = [$fournisseur->getIdFournisseur()];
         return $requete->execute($tableauInfos);
     }
-
-    private function getFournisseurRatings($fournisseurId) {
-        try {
-            $connexion = ConnexionBD::getInstance();
-            $requete = $connexion->prepare("SELECT note_globale FROM fournisseur WHERE id_fournisseur = ?");
-            $requete->bind_param("i", $fournisseurId);
-            $requete->execute();
-
-            $result = $requete->get_result();
-            $ratings = [];
-
-            while ($row = $result->fetch_assoc()) {
-                $ratings[] = $row['note_globale'];
-            }
-
-            return $ratings;
-        } catch (Exception $e) {
-            throw new Exception("Erreur lors de la récupération des notes du fournisseur : " . $e->getMessage());
-        }
-    }
 }
+
--- models/Fournisseur.class.php
+++ models/Fournisseur.class.php
@@ -1,31 +1,39 @@
 <?php
 
-class Fournisseur{
-    protected $id;
-    protected $nom;
-    protected $prenom;
-    protected $adresse; 
-    protected $telephone;
-    protected $username;
-    protected $password;
-    protected $photo_url;
-    protected $adresses;
+class Fournisseur {
+    private $id_fournisseur;
+    private $email;
     private $nom_de_la_compagnie;
-    private $note_globale = [];
+    private $nom_contact;
+    private $prenom_contact;
+    private $telephone;
+    private $username;
+    private $password;
+    private $photo_url;
+    private $note_globale;
 
-    public function __construct($id, $nom, $prenom, $adresse, $telephone, $username, $password, $photo_url, $nom_de_la_compagnie, $note_globale) {
-        $this->id = $id;
-        $this->nom = $nom;
-        $this->prenom = $prenom;
-        $this->adresse = $adresse;
-        $this->telephone = $telephone;
-        $this->username = $username;
-        $this->password = $password;
-        $this->photo_url = $photo_url;
-        $this->nom_de_la_compagnie = $nom_de_la_compagnie;
-        $this->note_globale = $note_globale;
+    // Constructor
+    public function __construct() {}
+
+    // Getter and Setter for id_fournisseur
+    public function getIdFournisseur() {
+        return $this->id_fournisseur;
     }
 
+    public function setIdFournisseur($id_fournisseur) {
+        $this->id_fournisseur = $id_fournisseur;
+    }
+
+    // Getter and Setter for email
+    public function getEmail() {
+        return $this->email;
+    }
+
+    public function setEmail($email) {
+        $this->email = $email;
+    }
+
+    // Getter and Setter for nom_de_la_compagnie
     public function getNomDeLaCompagnie() {
         return $this->nom_de_la_compagnie;
     }
@@ -34,6 +42,61 @@ class Fournisseur{
         $this->nom_de_la_compagnie = $nom_de_la_compagnie;
     }
 
+    // Getter and Setter for nom_contact
+    public function getNomContact() {
+        return $this->nom_contact;
+    }
+
+    public function setNomContact($nom_contact) {
+        $this->nom_contact = $nom_contact;
+    }
+
+    // Getter and Setter for prenom_contact
+    public function getPrenomContact() {
+        return $this->prenom_contact;
+    }
+
+    public function setPrenomContact($prenom_contact) {
+        $this->prenom_contact = $prenom_contact;
+    }
+
+    // Getter and Setter for telephone
+    public function getTelephone() {
+        return $this->telephone;
+    }
+
+    public function setTelephone($telephone) {
+        $this->telephone = $telephone;
+    }
+
+    // Getter and Setter for username
+    public function getUsername() {
+        return $this->username;
+    }
+
+    public function setUsername($username) {
+        $this->username = $username;
+    }
+
+    // Getter and Setter for password
+    public function getPassword() {
+        return $this->password;
+    }
+
+    public function setPassword($password) {
+        $this->password = $password;
+    }
+
+    // Getter and Setter for photo_url
+    public function getPhotoUrl() {
+        return $this->photo_url;
+    }
+
+    public function setPhotoUrl($photo_url) {
+        $this->photo_url = $photo_url;
+    }
+
+    // Getter and Setter for note_globale
     public function getNoteGlobale() {
         return $this->note_globale;
     }
@@ -41,22 +104,5 @@ class Fournisseur{
     public function setNoteGlobale($note_globale) {
         $this->note_globale = $note_globale;
     }
-
-    // Override toString method
-    public function __toString() {
-        return "Fournisseur: {$this->nom} {$this->prenom}, Compagnie: {$this->nom_de_la_compagnie}, Note Globale: {$this->note_globale}";
-    }
-
-    public function ajouterNote($rating) {
-        $this->note_globale[] = $rating;
-    }
-
-    public function calculeNoteGlobale() {
-        if (count($this->note_globale) > 0) {
-            $averageRating = array_sum($this->note_globale) / count($this->note_globale);
-            return round($averageRating, 2);
-        } else {
-            return 0;
-        }
-    }
 }
+
