22d89dc20b29ec5b74b0ff0a47458250ac0b028c
f-zahra <eva-200@hotmail.fr>
2024-02-14 14:34:17 -0500

    Update historique-utilisateur.php

--- views/templates/pages/historique-utilisateur.php
+++ views/templates/pages/historique-utilisateur.php
@@ -4,5 +4,6 @@
 <?php include($_SERVER['DOCUMENT_ROOT'] . "/Allo_Deneigement/views/templates/commons/custom-table.php"); ?>
 <?php include($_SERVER['DOCUMENT_ROOT'] . "/Allo_Deneigement/views/templates/commons/footer.php"); ?>
 <script src="<?php echo BASE_URL;?>/static/scripts/load-table.js"></script> 
+
 </body>
 </html>
