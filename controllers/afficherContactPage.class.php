5826ab0fb8d1a343bcf69f466f31058eca82153f
cylia93 <cylia.oudiai@outlook.com>
2024-02-18 21:41:01 -0500

    temp contact page controller

--- controllers/afficherContactPage.class.php
+++ controllers/afficherContactPage.class.php
@@ -2,27 +2,41 @@
 
 include_once(DOSSIER_BASE_INCLUDE . "controllers/controleur.abstract.class.php");
 include_once(DOSSIER_BASE_INCLUDE . "views/templates/commons/flash.php");
+include_once(DOSSIER_BASE_INCLUDE . "models/DAO/BilletDAO.class.php");
 
 class AfficherContactPage extends  Controleur
 {
+   
+
     public function __construct()
     {
         parent::__construct();
-    }
-
-    public function executerAction()
-    {
-        if ($this->acteur != "visiteur") {
-			array_push($this->messagesErreur, "Vous êtes déjà connécté.");
-			flash('Info', 'Vous êtes déjà connécté.', FLASH_WARNING);
-			return "landing-page";
-		}elseif{
-            flash('info', 'Vous etes sur la page de contacte.', FLASH_SUCCES);
-            return "ContactPage";
-        }else{
-            
-        }
-
        
     }
+
+    public function executerAction() {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            try {
+                $motif = $_POST['selectionMenu'] ?? '';
+                $texte = $_POST['info'] ?? '';
+                $email = $_POST['email'] ?? '';
+                $dateBillet = date('Y-m-d H:i:s'); // Récupérer la date actuelle
+    
+                echo "Motif: $motif, Texte: $texte, Email: $email, Date du billet: $dateBillet";
+                
+                $nouveauBillet = new Billet('', $motif, $texte, $dateBillet, $email); // Utiliser la date récupérée
+                BilletDAO::inserer($nouveauBillet);
+                
+                echo "Insertion réussie !";
+                flash('succes', 'Votre message a été envoyé avec succès', FLASH_SUCCESS);
+                return "landing-page";
+            } catch (Exception $e) {
+                flash('Erreur', 'Impossible d\'envoyer votre message. Veuillez vérifier vos informations.', FLASH_ERROR);
+                return "contactPage";
+            }
+        } else {
+            return "contactPage";
+        }
+    }
+    
 }
--- views/templates/pages/contactPage.php
+++ views/templates/pages/contactPage.php
@@ -5,158 +5,41 @@ if (!defined('DOSSIER_BASE_INCLUDE'))  define("DOSSIER_BASE_INCLUDE", "http://lo
 <?php include($_SERVER['DOCUMENT_ROOT'] . "/Allo_Deneigement/views/templates/commons/head.php"); ?>
 <?php include($_SERVER['DOCUMENT_ROOT'] . "/Allo_Deneigement/views/templates/commons/navbar.php"); ?>
 <div class="container-fluid my-6 p-3" style="width:60%;">
-    <form  id="signupForm" method="POST" action="?action=afficherContactPage" autocomplete="off">
+    <form  novalidate class="needs-validation" method="POST" action="?action=afficherContactPage">
         <fieldset>
             <legend>Formulaire de contacte</legend>
             <div class="container contact-page mx-auto p-2">
                 <div class="row mb-2 ">
                     <div class="col-md-6">
-                        <div class="mb-3">
-                            <label>Nom</label>
-                            <input type="text" id="usernames" class="form-control deneigement-validate-username" name="nom" required>
-                            <h5 id="usercheck" class="error-message usernameError">
-                                Veuillez saisir un nom valide.
-                            </h5>
-
-                        </div>
-
-                        <div class="mb-3">
-                            <label>Entreprise</label>
-                            <input type="text" class="form-control deneigement-validate-nomEntreprise" name="nomEntreprise">
-                            <h5 id="entepriseCheck" class="error-message  entrepriseError">
-                                Veuillez saisir un nom d'entreprise valide.
-                            </h5>
-
-                        </div>
-
-                        <div class="mb-3">
-                            <label>Courriel</label>
-                            <div class="input-group">
-
-                                <input type="email" class="form-control deneigement-validate-email" name="email" id="email" required>
-                                <h5 id="emailCheck" class="error-message  emailError">
-                                    Veuillez saisir un email valide.
-                                </h5>
-
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="col-md-6">
-                            <div class="mb-3">
-                                <label>Prenom</label>
-                                <input type="text" class="form-control deneigement-validate-firstname" id="fistNames" name="prenom" required>
-                                <h5 id="firstNameCheck" class="error-message  firstNameError">
-                                    Veuillez saisir un prenom valide.
-                                </h5>
-
-                            </div>
-
-
-
-                            <div class="mb-3">
-                                <label>Téléphone</label>
-                                <input type="tel" class="form-control deneigement-validate-phoneNumber" id="phone" name="telephone">
-                                <h5 id="phoneCheck" class="error-message  phoneError">
-                                    Veuillez saisir un numero de telephone valide.
-                                </h5>
-
-
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="row align-items-start">
-                        <div class="col-md-8">
-                            <label>Adresse</label>
-                            <input type="text" class="form-control deneigement-validate-adresse" id="adresse" name="adresse">
-                            <h5 id="adresseCheck" class="error-message adresseError">
-                                Veuillez saisir une adresse valide.
-                            </h5>
-                        </div>
-                        <div class="col-md-4">
-                            <label class="form-label deneigement-validate-selectionProvince">Province</label>
-                            <select class="form-select selectError" name="selectionProvince" required="">
-                                <option value="" disabled="" selected="">Sélectionnez une province</option>
-                                <option value="Alberta">AB</option>
-                                <option value="Colombie_Britanique">BC</option>
-                                <option value="Il-du-Prince-edouard">PE</option>
-                                <option value="Manitoba">MB</option>
-                                <option value="Nouveau-Brunswick">NB</option>
-                                <option value="Nouvelle-ecosse">NS</option>
-                                <option value="Ontario">ON</option>
-                                <option value="Quebec">QC</option>
-                                <option value="Saskatchewan">SK</option>
-                                <option value="Terre-Neuve-et-Labrador">NL</option>
-                                <option value="Nuvavut">NU</option>
-                                <option value="Territoure du Nord-Ouest">NT</option>
-                                <option value="Yukon">YT</option>
-                            </select>
-                            <h5 id="selectMenuCheck" class="error-message selectProvinceError">
-                                Veuillez choisir une province.
-                            </h5>
-
-                        </div>
-                    </div>
-
-                    <div class="row">
-                        <div class="col-md-6 mb-3">
-                            <ul class="nav flex-column contact-page">
-                                <li class="nav-item">
-                                    <label>Ville</label>
-                                    <input type="text" class="form-control deneigement-validate-ville" id="ville" name="ville">
-                                    <h5 id="villeCheck" class="error-message villeError">
-                                        Veuillez saisir une ville.
-                                    </h5>
-
-                                </li>
-                            </ul>
-                        </div>
-                        <div class="col-md-3 mb-3">
-                            <label>Pays</label>
-                            <input type="text" class="form-control deneigement-validate-pays" id="pays" name="pays">
-                            <h5 id="paysCheck" class="error-message paysError">
-                                Veuillez saisir un pays
-                            </h5>
-
-                        </div>
-                        <div class="col-md-3 mb-3">
-                            <ul class="nav flex-column contact-page">
-                                <li class="nav-item">
-                                    <label>Code Postal</label>
-                                    <input type="text" class="form-control deneigement-validate-codePostal" id="codePostal" name="codePostal">
-                                    <h5 id="codePostalCheck" class="error-message codePostalError">
-                                        Veuillez saisir un code postal valide.
-                                    </h5>
-
-                                </li>
-                            </ul>
-                        </div>
+                    
+                    <div class="form-floating text-uppercase">
+                <input type="email" name="email" class="form-control border border-3 border-light rounded-0 mb-2" id="floatingInput" placeholder="name@example.com" required>
+                <div class="invalid-feedback">
+                    Saississez une adresse courriel valide.
+                </div>
+            </div>
                     </div>
 
                     <div class="mb-3">
-                        <label class="form-label deneigement-validate-selectionMenu">Sélectionnez un problème :</label>
-                        <select class="form-select selectError" name="selectionMenu" required="">
-                            <option value="" disabled="" selected="">Sélectionnez un problème</option>
-                            <option value="probleme_technique">Problème technique</option>
-                            <option value="delai_expi">Délai expiré</option>
-                            <option value="retard_service">Retard dans le service</option>
-                            <option value="mauvaise_communication">Mauvaise communication</option>
-                            <option value="endommagement_propriete">Endommagement de la propriété</option>
-                            <option value="couts_imprevus">Coûts imprévus</option>
-                            <option value="probleme_contractuels">Problème contractuel</option>
-                            <option value="manque_flexibilite">Manque de flexibilité</option>
-                            <option value="manque_professionnalisme">Manque de professionnalisme</option>
-                        </select>
-                        <h5 id="selectMenuCheck" class="error-message selectError">
-                            Veuillez choisir un probleme parmis la selection.
-                        </h5>
-
-                    </div>
+                       <label class="form-label deneigement-validate-selectionMenu">Sélectionnez un problème :</label>
+                       <select class="form-select selectError" name="selectionMenu" required>
+                       <option value="" disabled selected>Sélectionnez un problème</option>
+                       <option value="probleme_technique">Problème technique</option>
+                       <option value="delai_expi">Délai expiré</option>
+                       <option value="retard_service">Retard dans le service</option>
+                       <option value="mauvaise_communication">Mauvaise communication</option>
+                       <option value="endommagement_propriete">Endommagement de la propriété</option>
+                       <option value="couts_imprevus">Coûts imprévus</option>
+                       <option value="probleme_contractuels">Problème contractuel</option>
+                       <option value="manque_flexibilite">Manque de flexibilité</option>
+                       <option value="manque_professionnalisme">Manque de professionnalisme</option>
+                       </select>
+                        <div class="invalid-feedback">Veuillez sélectionner un problème.</div>
+                         </div>
 
                     <div>
                         <label id="information" class="form-label ">Autre Informations/Remarques.</label>
-                        <textarea class="form-control" rows="4" cols="50" maxlength="100"></textarea>
+                        <textarea class="form-control" name="info" placeholder="Veuillez saisir le maximum d'informations possible" rows="4" cols="50" maxlength="500"></textarea>
                     </div>
 
                     <div class="d-flex justify-content-center">
@@ -168,7 +51,7 @@ if (!defined('DOSSIER_BASE_INCLUDE'))  define("DOSSIER_BASE_INCLUDE", "http://lo
     </form>
 </div>
 <?php include($_SERVER['DOCUMENT_ROOT'] . "/Allo_Deneigement/views/templates/commons/footer.php"); ?>
-<script src="<?php echo DOSSIER_VIEWS; ?>static/scripts/contactPage-validation.js"></script>
+<script src="<?php echo BASE_URL_VIEWS; ?>static/scripts/css-validation.js"></script>
 </body>
 
 </html>
