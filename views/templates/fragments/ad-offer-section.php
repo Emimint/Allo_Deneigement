630e18b1fd14a6f55f307d44096493429dde931c
f-zahra <eva-200@hotmail.fr>
2024-02-21 14:48:35 -0500

    Corrected presentation erros

--- views/templates/fragments/ad-offer-section.php
+++ views/templates/fragments/ad-offer-section.php
@@ -2,6 +2,18 @@
 if (!defined('BASE_URL_VIEWS')) define('BASE_URL_VIEWS', 'http://localhost:80/Allo_Deneigement/views/');
 if (!defined('DOSSIER_BASE_INCLUDE'))  define("DOSSIER_BASE_INCLUDE", "http://localhost:80/Allo_Deneigement/");
 ?>
+
+<style>
+
+  #item-card{
+    width: 280px;
+   
+  }
+  #items{
+    margin-left: 5%;
+    margin-right: 5%;
+  }
+</style>
 <div class="container-fluid">
   <section class="masthead text-black" style="background-image: linear-gradient(rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.5)), url('<?php echo BASE_URL_VIEWS; ?>/static/image/snow-plow.jpg'); background-size: cover; background-position: center;">
     <div class="container">
@@ -13,63 +25,83 @@ if (!defined('DOSSIER_BASE_INCLUDE'))  define("DOSSIER_BASE_INCLUDE", "http://lo
   <div class="ad-service container-fluid">
     <div class="container p-5">
       <!--      Principaux services du fournisseur:-->
-      <div class="row">
-    <?php foreach ($controleur->getOffre() as $offre): ?>
-        <div class="col-lg-4 col-md-12 mb-4">
-            <div class="card h-100 shadow-lg">
-                <div class="card-body">
-                    <div class="text-center p-3">
-                        <h5 class="card-title"><?php echo $offre->getDescription(); ?></h5>
-                        <br><br>
-                        <span class="h2"><?php echo $offre->getPrixUnitaire(); ?></span>/heure
-                        <br><br>
+<div id="carouselExample" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner">
+        <?php
+        $first = true;
+        foreach ($controleur->getOffre() as $offre):
+        ?>
+        <div class="carousel-item <?php echo $first ? 'active' : ''; ?>">
+            <div class="row" id="items">
+                <div class="col-lg-4 col-md-12 mb-4">
+                    <div class="card h-100 shadow-lg" id="item-card">
+                        <div class="card-body">
+                            <div>
+                                <h5 class="card-title"><?php echo $offre->getDescription(); ?></h5>
+                                <br><br>
+                                <span class="h2"><?php echo $offre->getPrixUnitaire(); ?></span>/heure
+                                <br><br>
+                            </div>
+                            <p class="card-text"><?php echo $offre->getDescription(); ?></p>
+                        </div>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item <?php echo ($offre->getTypeClientele() === 'Résidentiel') ? 'active' : ''; ?>">
+                                <i class="fa <?php echo ($offre->getTypeClientele() === 'Résidentiel') ? 'fa-check' : 'fa-times'; ?>" aria-hidden="true"></i>
+                                Résidentiel
+                            </li>
+                            <li class="list-group-item <?php echo ($offre->getTypeClientele() === 'Commercial') ? 'active' : ''; ?>">
+                                <i class="fa <?php echo ($offre->getTypeClientele() === 'Commercial') ? 'fa-check' : 'fa-times'; ?>" aria-hidden="true"></i>
+                                Commercial
+                            </li>
+                        </ul>
+                        <div class="card-body text-center">
+                            <a href="http://localhost:80/Allo_Deneigement/views/templates/pages/soumission-offre.php" class="btn btn-lg">Selectionner</a>
+                        </div>
                     </div>
-                    <p class="card-text"><?php echo $offre->getDescription(); ?></p>
-                </div>
-             
-                    <ul class="list-group list-group-flush">
-    <li class="list-group-item <?php echo ($offre->getTypeClientele() === 'Résidentiel') ? 'active' : ''; ?>">
-        <i class="fa <?php echo ($offre->getTypeClientele() === 'Résidentiel') ? 'fa-check' : 'fa-times'; ?>" aria-hidden="true"></i>
-        Résidentiel
-    </li>
-    <li class="list-group-item <?php echo ($offre->getTypeClientele() === 'Commercial') ? 'active' : ''; ?>">
-        <i class="fa <?php echo ($offre->getTypeClientele() === 'Commercial') ? 'fa-check' : 'fa-times'; ?>" aria-hidden="true"></i>
-        Commercial
-    </li>
-   
-                    </ul>
-
-                <div class="card-body text-center">
-                    <a href="http://localhost:80/Allo_Deneigement/views/templates/pages/soumission-offre.php" class="btn btn-lg">Selectionner</a>
                 </div>
             </div>
         </div>
-    <?php endforeach; ?>
+        <?php
+        $first = false;
+        endforeach;
+        ?>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
 </div>
+
 
       <!--      Autres services du fournisseurs-->
       <div class="row mt-2">
-        <h3>Autres services</h3>
-        <div class="accordion accordion-flush" id="accordionFlushExample">
-        <?php foreach ($controleur->getOffre() as $offre): ?>
-          <div class="accordion-item border rounded">
-            <h2 class="accordion-header" id="flush-headingOne">
-              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-              <?php echo $offre->getCategorie(); ?>
-              </button>
-            </h2>
-            <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
-              <div class="accordion-body"> <?php echo $offre->getDescription(); ?>
-                <br><br>
-                <div class="d-flex flex-row-reverse">
-                  <a href="?=action=ToBeDeterminated" class="btn btn-danger text-end">Selectionner</a>
+    <h3>Autres services</h3>
+    <div class="accordion accordion-flush" id="accordionFlushExample">
+        <?php foreach ($controleur->getOffre() as $index => $offre): ?>
+            <div class="accordion-item border rounded">
+                <h2 class="accordion-header" id="flush-heading<?php echo $index; ?>">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse<?php echo $index; ?>" aria-expanded="false" aria-controls="flush-collapse<?php echo $index; ?>">
+                        <?php echo $offre->getCategorie(); ?>
+                    </button>
+                </h2>
+                <div id="flush-collapse<?php echo $index; ?>" class="accordion-collapse collapse" aria-labelledby="flush-heading<?php echo $index; ?>" data-bs-parent="#accordionFlushExample">
+                    <div class="accordion-body">
+                        <?php echo $offre->getDescription(); ?>
+                        <br><br>
+                        <div class="d-flex flex-row-reverse">
+                            <a href="?action=ToBeDetermined" class="btn btn-danger text-end">Selectionner</a>
+                        </div>
+                    </div>
                 </div>
-              </div>
             </div>
-          </div>
-          <?php endforeach ?>
-        </div>
-      </div>
+        <?php endforeach ?>
+    </div>
+</div>
+
     </div>
 
     <div>
--- views/templates/fragments/customer-review-section.php
+++ views/templates/fragments/customer-review-section.php
@@ -4,56 +4,60 @@ if (!defined('DOSSIER_BASE_INCLUDE'))  define("DOSSIER_BASE_INCLUDE", "http://lo
 ?>
 <div class="container text-black p-3">
   <div class="container text-center my-3">
-    <div class="row">
-      <div id="recipeCarousel" class="carousel slide w-100">
+  <div class="row">
+    <div id="recipeCarousel" class="carousel slide w-100">
         <div class="carousel-inner w-100 mt-5" role="listbox">
-          <?php foreach( $controleur->getReviews() as $review):?>
-          <div class="carousel-item active">
-            <div class="col-md-4">
-              <div class="card" style="width: 18rem;">
-                <img src="<?php echo BASE_URL_VIEWS; ?>static/image/avatar.svg" style="width: 70px;height: 70px;" class="img-fluid img-thumbnail mx-auto rounded-circle mt-5 border border-2 border-dark" alt="avatar">
-                <div class="card-body">
-                 
-                 <h5 class="card-title"><?php echo   $controleur->getUserName($review->getIdUtilisateur()) ?></h5> 
-                  <div class="d-flex justify-content-center mb-3">
-                    <div class="content text-center">
-                    <div class="ratings">
-    <span class="product-rating"><?php echo $review->getScore()?></span><span>/5</span>
-    <div class="stars d-flex flex-nowrap">
-        <?php
-        $rating = $review->getScore();
-        for ($i = 1; $i <= 5; $i++) {
-            if ($i <= $rating) {
-                echo '<i class="fa fa-star"></i>';
-            } else {
-                echo '<i class="fa fa-star-o"></i>'; // Assuming you have an "empty" star icon
-            }
-        }
-        ?>
+            <?php
+            $firstItem = true;
+            foreach ($controleur->getReviews() as $review):
+            ?>
+                <div class="carousel-item <?php echo $firstItem ? 'active' : ''; ?>">
+                    <div class="col-md-4">
+                        <div class="card" style="width: 18rem;">
+                            <img src="<?php echo BASE_URL_VIEWS; ?>static/image/avatar.svg" style="width: 70px;height: 70px;" class="img-fluid img-thumbnail mx-auto rounded-circle mt-5 border border-2 border-dark" alt="avatar">
+                            <div class="card-body">
+                                <h5 class="card-title"><?php echo $controleur->getUserName($review->getIdUtilisateur()) ?></h5>
+                                <div class="d-flex justify-content-center mb-3">
+                                    <div class="content text-center">
+                                        <div class="ratings">
+                                            <span class="product-rating"><?php echo $review->getScore() ?></span><span>/5</span>
+                                            <div class="stars d-flex flex-nowrap">
+                                                <?php
+                                                $rating = $review->getScore();
+                                                for ($i = 1; $i <= 5; $i++) {
+                                                    if ($i <= $rating) {
+                                                        echo '<i class="fa fa-star"></i>';
+                                                    } else {
+                                                        echo '<i class="fa fa-star-o"></i>'; // Assuming you have an "empty" star icon
+                                                    }
+                                                }
+                                                ?>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <p class="card-text"><?php echo $review->getCommentaire() ?></p>
+                                <a href="#" class="btn">Savoir plus <i class="fa-solid fa-chevron-right"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <?php
+                $firstItem = false; // Set to false after the first iteration
+            endforeach;
+            ?>
+        </div>
+        <button class="carousel-control-prev w-auto" type="button" data-bs-target="#recipeCarousel" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon bg-dark border border-dark rounded-circle" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next w-auto" type="button" data-bs-target="#recipeCarousel" data-bs-slide="next">
+            <span class="carousel-control-next-icon bg-dark border border-dark rounded-circle" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
     </div>
 </div>
 
-                    </div>
-                  </div>
-                  <p class="card-text"><?php echo $review->getCommentaire()?></p>
-                  <a href="#" class="btn">Savoir plus <i class="fa-solid fa-chevron-right"></i></a>
-                </div>
-              </div>
-            </div>
-          </div>
-         
-          <?php endforeach?>
-        </div>
-        <button class="carousel-control-prev w-auto" type="button" data-bs-target="#recipeCarousel" data-bs-slide="prev">
-          <span class="carousel-control-prev-icon bg-dark border border-dark rounded-circle" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next w-auto" type="button" data-bs-target="#recipeCarousel" data-bs-slide="next">
-          <span class="carousel-control-next-icon bg-dark border border-dark rounded-circle" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
-        </button>
-      </div>
-    </div>
   </div>
   <script src="<?php echo BASE_URL_VIEWS; ?>static/scripts/review-carousel.js"></script>
 </div>
